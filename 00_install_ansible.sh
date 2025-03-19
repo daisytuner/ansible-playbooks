030b22c2ae8e596be21a9bec371b6c8122ddefa3
Lukas Truemper <lukas.truemper@outlook.de>
2025-03-19 16:57:32 +0100

    Minor updates and coral tpu playbook

--- 00_install_ansible.sh
+++ 00_install_ansible.sh
@@ -1,5 +1,4 @@
-for i in {30..42}
+for i in {20..23}
 do
-    ssh -t "daisy@192.168.0.$i" 'sudo apt-get install -y python3-pip && echo "export PATH=/home/daisy/.local/bin:$PATH" >> /home/daisy/.bashrc && pip install ansible --break-system-packages'
+    ssh -t "daisy@192.168.0.$i" 'sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get install -y python3-pip && echo "export PATH=/home/daisy/.local/bin:$PATH" >> /home/daisy/.bashrc && pip install ansible --break-system-packages'
 done
-
